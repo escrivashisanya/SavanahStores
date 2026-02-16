375eac4a7aca4d069d3d6fb7874001ea1a40c8fc
escrivashisanya <josemariaindangasi@gmail.com>
2026-02-16 10:41:00 +0300

    sixth commit

---
+++ storeapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from storeapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.index),
+    path('gallery/', views.gallery),
+]
